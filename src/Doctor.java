f0406e9cff7c8d5e33899e40141999440f78ca9a
JúniorAL930305 <a0079-junior@hotmail.com>
2022-08-16 16:33:52 -0500

    se practica la sobre carga, sobre escritura y Herencia

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,26 +1,71 @@
+import java.util.ArrayList;
+import java.util.Date;
 
-public class Doctor {
+public class Doctor extends  User {
     //Atributos
-    static int id = 0; //Autoincrement
-    String name;
-    String speciality;
-
-    Doctor(){
-        System.out.println("Construyendo el Objeto Doctor");
-        id++;
+    private String speciality;
+    Doctor(String name, String email){
+        super(name,email);
+        System.out.println("El nombre del doctor asignado es "+ name+"  y su especialidad es "+speciality);
+        this.speciality=speciality;
+    }
+    public String getSpeciality() {
+        return speciality;
+    }
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+    public void setAvailableAppointments(ArrayList<AvailableAppointment> availableAppointments) {
+        this.availableAppointments = availableAppointments;
     }
 
-    Doctor(String name){
-        System.out.println("El nombre del Doctor asignado es: " + name);
+    ArrayList<AvailableAppointment> availableAppointments=new ArrayList<>();
+    public void  addAvailableAppointment(Date date, String time){ // añade nuevas citas deisponibles que se acumulan en el array de debajo
+        availableAppointments.add(new Doctor.AvailableAppointment(date,time));
+    }
+    public  ArrayList<AvailableAppointment>getAvailableAppointments(){
+        return availableAppointments;
     }
 
-    //Comportamientos
-    public void showName(){
-        System.out.println(name);
+    public void availableAppointments(String s) {
     }
 
-    public void showId(){
-        System.out.println("ID Doctor: " + id);
+    public static class AvailableAppointment{
+        //Available Appointment
+        private  int id;
+        private Date date;
+        private  String time;
+
+
+
+        public AvailableAppointment(Date date, String time) { // metodo cosntructor
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 
 }
